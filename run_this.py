297c39d40173a8ba008147ea0fb394222d4bb464
ivanchik3222 <evanifrimov@gmail.com>
2024-05-25 12:37:04 +0500

    2_0 update , add multyprocessing

--- run_this.py
+++ run_this.py
@@ -1,6 +1,7 @@
 from g4f.client import Client # gpt без огранечений
 import json
 from googletrans import Translator # библиотека для перевода от гугла
+from multiprocessing import Pool
 
 translator = Translator()#создаём объект
 
@@ -9,20 +10,36 @@ client = Client()#необхоимость как на 2 строчки выше
 with open("cards.json","r", encoding="utf-8") as file:
     base = json.load(file)
 
-for i in base:
-    print(i, end = ":\n") #выводит   card1: 
-    for j in base[i]:
+
+def func(card_id):
+    result = []
+    for j in base["card"+str(card_id+1)]:
         
         response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": "очень кратко в пару слов ответь на  вопрос: " + j},],
+            model="gpt-4-turbo",
+            messages=[{"role": "user", "content": "очень кратко в пару слов ответь на  вопрос: " + translator.translate(j, src='kk', dest='en').text},],
         ) #запрос 
 
-        print("вопрос " + str(base[i].index(j) + 1) + ": " + j)# выводит вопрос
+        result.append(["вопрос " + str(base["card"+str(card_id+1)].index(j) + 1) + ": " + j  ,  response.choices[0].message.content])
 
-        # print("Ответ(рус):",translator.translate(response.choices[0].message.content, src='kk', dest='ru').text) тут часто гугл ломается
-        print("ответ :",response.choices[0].message.content)#ответ
+    return result
 
-        print("")#декорация
 
-input("нажмите enter для выхода")#что бы сразу не выходило из консоли
+
+# print(func(0))
+
+if __name__ == '__main__':
+
+    with Pool(5) as p:
+        list_of_out = p.map(func, range(13))
+    with open("result.txt", "w", encoding="utf-8") as out_file:
+        for i in list_of_out:
+            print("card" + str(list_of_out.index(i) + 1) + ":")
+            out_file.write("card" + str(list_of_out.index(i) + 1) + ":" + "\n")
+            for j in i:
+                print(j[0])
+                print("ответ:",j[1])
+                print()
+                out_file.write(j[0]+"\n")
+                out_file.write("ответ:" + j[1] + "\n")
+                out_file.write("\n")
